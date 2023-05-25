e30281222b7346a6f19d519ca59ea64d17351b28
Natallia Shliapik <nshliapik@gmail,com>
2023-05-25 12:34:40 +0300

    BR2JSX

---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class BR2JSX extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+  render() {
+    const textArr = this.props.text.split(/<br\s*\/?>/g); //или /<br  *\/?>/
+    const content = [];
+    for (let i = 0; i < textArr.length; i++) {
+      if (i) {
+        content.push(<br />);
+      }
+      content.push(textArr[i]);
+    }
+
+    return <div className='BR2JSX'>{content}</div>;
+  }
+}
+
+export default BR2JSX;
